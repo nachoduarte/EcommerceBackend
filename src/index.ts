780a44397ddd290c87aef58bd9d4b124714b6ad8
Nacho Duarte <nacho.duarte.97@gmail.com>
2022-10-04 19:29:19 -0300

    added arquitectura daos y dtos, persistencia, validators y errorHandler

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,56 @@
-require('dotenv').config()
-import './store/mongoDb/connection' // connect to MongoDB
-// import './store/firebase/connection' // connect to Firebase
-
-import express from 'express'
-import config from '../config'
-import authRouter from './api/auth/controller'
-import productsRouter from './api/products/controller'
-import shoppingCartsRouter from './api/shoppingCarts/controller'
-import { handleUnknownRoutes } from './network/errors'
-import { errorHandler } from './network/errors'
-import swaggerUI from 'swagger-ui-express'
-const swaggerDoc = require('./api/swagger.json')
-
-const app = express()
-app.use(express.json())
-app.use('/api/auth', authRouter)
-app.use('/api/products', productsRouter)
-app.use('/api/shoppingCarts', shoppingCartsRouter)
-app.use('/api/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
-app.use(errorHandler)
-app.use('*', handleUnknownRoutes)
-
-app.listen(config.API_PORT, () => {
-  console.log(`Server open on PORT: ${config.API_PORT}`)
+require('dotenv').config({
+  path: `.env.${process.argv[2].split('=')[1] || 'development'}`,
 })
+import config from '../config'
+if (config.PERSISTENCE_SYSTEM === 'mongo') import('./store/mongoDb/connection')
+import express from 'express'
+import { handleUnknownRoutes } from './middleware/routes/handleUnknownRoutes'
+import { errorHandler } from './middleware/errors/errorHandler'
+import swaggerUI from 'swagger-ui-express'
+import { swaggerSpecs } from '../swaggerSpecs'
+import cluster from 'cluster'
+import os from 'os'
+import cors from 'cors'
+import logger from './logger'
+import morgan from 'morgan'
+import routes from './routes/index'
+
+if (config.API_CLUSTER && cluster.isPrimary) {
+  const numCpus = os.cpus().length
+
+  logger.info('SERVIDOR MAESTRO DEL CLUSTER: ')
+  logger.info('Número de procesadores: ' + numCpus)
+  logger.info('PID:' + process.pid)
+
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker) => {
+    logger.info('Worker ' + process.pid + ' exit')
+    cluster.fork()
+  })
+} else {
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(
+    morgan('combined', {
+      stream: { write: (message) => logger.info(message.trim()) },
+    })
+  )
+  app.use('/public', express.static('storage'))
+  app.use('/api', routes)
+  app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs))
+  app.use(errorHandler)
+  app.use('*', handleUnknownRoutes)
+
+  app.listen(config.API_PORT, () => {
+    logger.info(
+      `Server open on PORT: ${config.API_PORT} - PID(${
+        process.pid
+      }) - (${new Date().toLocaleString()})`
+    )
+  })
+}
---
+++ src/persistence/repository/ProductRepository.ts
@@ -0,0 +1,35 @@
+import ProductDaoFactory from '../dao/productDao/ProductDaoFactory'
+
+import { ProductDataType, ProductType } from '../../types/ProductType'
+
+
+export default class ProductRepository {
+  dao: any
+
+  constructor() {
+    this.dao = new ProductDaoFactory().getDao()
+  }
+
+  async getAll(): Promise<ProductType[]> {
+    return this.dao.getAll()
+  }
+
+  async getById(id: string): Promise<ProductType | null> {
+    return this.dao.getById(id)
+  }
+
+  async save(obj: ProductDataType): Promise<ProductType> {
+    return this.dao.save(obj)
+  }
+
+  async updateById(
+    id: string,
+    newObj: ProductDataType
+  ): Promise<ProductType | null> {
+    return this.dao.updateById(id, newObj)
+  }
+
+  async deleteById(id: string): Promise<String | null> {
+    return this.dao.deleteById(id)
+  }
+}
